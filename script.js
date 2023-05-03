661a60b5b8e84500a1b64f540b55e080f747d981
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2023-05-03 17:23:53 +0100

    learning about functions

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+*/
+
+function logger() {
+  console.log('My name is Jonas');
+}
+
+
+logger();
