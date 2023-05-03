9d22ca19755f9533ade6fdc8f98d2e68c112b3f9
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2023-05-03 22:14:01 +0100

    learnt about functions

--- script.js
+++ script.js
@@ -8,8 +8,22 @@ if (hasDriversLicense) console.log('I can drive');
 */
 
 function logger() {
-  console.log('My name is Jonas');
+  console.log('My name is Adebisi');
 }
 
 
 logger();
+logger();
+logger();
+
+function fruitProcessors(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessors(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessors(5, 0));
+
+const appleOrangeJuice = fruitProcessors(2, 4);
+console.log(appleOrangeJuice);
