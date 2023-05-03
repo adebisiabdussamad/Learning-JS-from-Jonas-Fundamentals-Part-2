ddc921259f92e8c01bb0a1d684170ffd385bfd26
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2023-05-03 22:44:51 +0100

    learnt about function declaration and expression

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const passTest = true;
 if (passTest) hasDriverLicense = true;
 if (hasDriversLicense) console.log('I can drive');
 */
-
+/*
 function logger() {
   console.log('My name is Adebisi');
 }
@@ -23,7 +23,24 @@ function fruitProcessors(apples, oranges) {
 
 const appleJuice = fruitProcessors(5, 0);
 console.log(appleJuice);
-// console.log(fruitProcessors(5, 0));
+ console.log(fruitProcessors(5, 0));
 
 const appleOrangeJuice = fruitProcessors(2, 4);
 console.log(appleOrangeJuice);
+*/
+
+//FUNCTION DECLARATIONS
+
+function calAge1(birthYear) {
+  return 2037 -  birthYear;
+}
+const age1 = calAge1(1991);
+
+
+//FUNCTION EXPRESSION
+const calAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calAge1(1991);
+
+console.log(age1, age2);
