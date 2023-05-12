1a0278f96fb31e8ccec1a7b695ed045b24c8a965
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2023-05-12 06:59:44 +0100

    completed code-challenge number 2

--- code-challege.js
+++ code-challege.js
@@ -80,3 +80,26 @@ values (so don't store the tip values in separate variables first, but right in 
 array) 😉
 GOOD LUCK 😀
  */
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.20;
+  }
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips[0]);
+console.log(tips[1]);
+console.log(tips[2]);
+
+const total = [
+  [bills[0] + tips[0]],
+  [bills[1] + tips[1]],
+  [bills[2] + tips[2]],
+];
+
+console.log(total[0]);
+console.log(total[1]);
+console.log(total[2]);
